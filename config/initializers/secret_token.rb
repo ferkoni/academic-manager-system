4f337ceb3433b7fc6d7aab420b9ea4ee0a0d736d
afaraldo <iceberg.04@gmail.com>
2013-08-06 23:41:57 -0400

    I added basic structure projec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AcademicManagerSystem::Application.config.secret_token = 'da7c1f2d417491d332cc9faac12eef362a2f6b5e9c9592830ffb4877b57e0863a36753e5345e3832b818a26a73a4411ee57ca1beeafda8f35454de653c52bc33'
